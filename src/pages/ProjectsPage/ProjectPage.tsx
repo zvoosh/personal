2dca1739857f3f01f4c8cdca2e0ab1abf7dd2690
Dusan Ilic <dusan.ilic1999@gmail.com>
2025-11-05 15:59:21 +0100

    fix: fix

--- src/pages/ProjectsPage/ProjectPage.tsx
+++ src/pages/ProjectsPage/ProjectPage.tsx
@@ -6,13 +6,15 @@ const ProjectsPage = () => {
   return (
     <main className="w-100 h-100 flex justify-center  pt-2 text-white font-space">
       <div className="container">
-        <h1 className="font-gaming font-30 text-yellow">Projects</h1>
-        <p className="mt-1 font-16 pl-5">
-          Here are some of the projects I’ve worked on recently. 
-        </p>
-        <div className="w-100 flex justify-center mt-3">
-          <div className="w-75">
-            <div className="project-card p-2 mb-4">
+        <div className="hello-padding">
+          <h1 className="font-gaming font-30 text-yellow">Projects</h1>
+          <p className="mt-1 font-16 pl-1">
+            Here are some of the projects I’ve worked on recently.
+          </p>
+        </div>
+        <div className="w-100 flex justify-center">
+          <div className="width">
+            <div className="p-2 mb-4">
               <img
                 src={project1}
                 alt="Project 1"
@@ -33,7 +35,7 @@ const ProjectsPage = () => {
                 View Project
               </a>
             </div>
-            <div className="project-card p-2 mb-4">
+            <div className="p-2 mb-4">
               <img
                 src={project2}
                 alt="Project 2"
@@ -54,13 +56,15 @@ const ProjectsPage = () => {
                 View Project
               </a>
             </div>
-            <div className="project-card p-2 mb-4">
+            <div className="p-2 mb-4">
               <img
                 src={project3}
                 alt="Project 3"
                 className="w-100 h-100 cover mb-2"
               />
-              <h2 className="font-28 mb-1 font-gaming">Student-Teacher Scheduler</h2>
+              <h2 className="font-28 mb-1 font-gaming">
+                Student-Teacher Scheduler
+              </h2>
               <p className="mb-2 font-12">
                 My personal portfolio showcasing projects, skills, and
                 experience. Built with React, Tailwind, and Ant Design for a
