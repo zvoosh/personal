8e222a3b7c87bf56ffd687c333e4bce84e7ae08b
Dusan Ilic <dusan.ilic1999@gmail.com>
2026-02-01 15:59:17 +0100

    projects

--- src/pages/ProjectsPage/ProjectsPage.tsx
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,30 +1,39 @@
 import expenseTrack from "../../assets/expenseTrack.png";
-import gallery from "../../assets/mygallery.png";
+import anotherPortfolio from "../../assets/another-portfolio.png";
 import mypetlite from "../../assets/mypetlite.png";
 import supermall from "../../assets/supermall3.png";
-import todo from "../../assets/todo.png";
+import recipes from "../../assets/recipes.png";
+import appointment from "../../assets/appointment.png";
 
 const ProjectsPage = () => {
   return (
-    <main style={{maxWidth:"100vw"}}>
-      <div className="container">
+    <main style={{maxWidth:"100vw", padding: "24px", display: "flex", justifyContent: "center"}}>
+      <div className="container font-space">
         <div className="project-grid">
-            <div className="project-card">
+            <a target="_blank" className="project-card" href="https://expensetrack.dusanprogram.eu/">
               <img src={expenseTrack} style={{objectFit: "contain"}}/>
-              dusan ilic
-            </div>
-            <div className="project-card">
-              <img src={gallery} style={{objectFit: "contain"}}/>
-            </div>
-            <div className="project-card">
+              <p className="project-title">Expense Tracker</p>
+            </a>
+            <a target="_blank" className="project-card" href="https://portfolio.dusanprogram.eu/">
+              <img src={anotherPortfolio} style={{objectFit: "contain"}}/>
+              <p className="project-title">Another Portfolio</p>
+            </a>
+            <a target="_blank" className="project-card" href="https://mypetlite.dusanprogram.eu/">
               <img src={mypetlite} style={{objectFit: "contain"}}/>
-            </div>
-            <div className="project-card">
+              <p className="project-title">My pet lite</p>
+            </a>
+            <a target="_blank" className="project-card" href="https://supermall.dusanprogram.eu/">
               <img src={supermall} style={{objectFit: "contain"}}/>
-            </div>
-            <div className="project-card">
-              <img src={todo} style={{objectFit: "contain"}}/>
-            </div>
+              <p className="project-title">Supermall Fullstack Application</p>
+            </a>
+            <a target="_blank" className="project-card" href="https://recipes.dusanprogram.eu/">
+              <img src={recipes} style={{objectFit: "contain"}}/>
+              <p className="project-title">Explore Recipe Full stack Application</p>
+            </a>
+            <a target="_blank" className="project-card" href="https://apointments.dusanprogram.eu/page/home">
+              <img src={appointment} style={{objectFit: "contain"}}/>
+              <p className="project-title">Appointment Booking Application</p>
+            </a>
         </div>
       </div>
     </main>
